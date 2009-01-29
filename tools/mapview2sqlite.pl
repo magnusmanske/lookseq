0515a4cd299bd6d885be90748aed0dd8905fa2b4
Magnus Manske <magnusmanske@users.noreply.github.com>
2009-01-29 14:18:21 +0000

    better command line options, support for 'forced' single chromosome

--- tools/mapview2sqlite.pl
+++ tools/mapview2sqlite.pl
@@ -3,11 +3,48 @@
 use strict ;
 use warnings ;
 
-my ( $reference , $mapview , $fragment , $variance , $from , $to ) = @ARGV ; # $from, $to are optional
+my ( $reference , $mapview , $fragment , $variance , $from , $to , $force_single_chromosome ) ;
+
+# Default
+$fragment = 500 ;
+$variance = 500 ;
+
+use Getopt::Long;
+GetOptions ( 'reference=s' => \$reference ,
+			 'mapview=s' => \$mapview ,
+			 'fragment=i' => \$fragment ,
+			 'variance=i' => \$variance ,
+			 'from=i' => \$from ,
+			 'to=i' => \$to ,
+			 'fsc' => \$force_single_chromosome
+			 ) ;
+
+my $usage ="USAGE: mapview2sqlite [parameters] | sort | sqlite3 database.sqlite
+--reference=FILE	Reference sequence (FASTA) [mandatory]
+--mapview=FILE		Mapview file [mandatory]
+--fragment			Fragment size (default:500) [optional]
+--variance			Variance size (default:500) [optional]
+--from				Start coordinates (default:1) [optional]
+--to				End coordinates (default:length of largest chromosome) [optional]
+--fsc				Force single chromosome (deactivates chromosome name checking;
+						all reads will be mapped to the single chromosome in the reference) [optional]
+" ;
+
+die $usage unless defined $reference ;
+die $usage unless defined $mapview ;
+
 my %chrs ;
 
 # Read reference genome
 get_reference_from_file ( $reference ) ;
+
+die "No reference sequence found in $reference.\n" if scalar ( keys %chrs ) == 0 ;
+die "Multiple chromosomes, but FSC set - aborting\n" if $force_single_chromosome and scalar ( keys %chrs ) > 1 ;
+my $default_chromosome ;
+if ( $force_single_chromosome ) {
+	my @a = keys %chrs ;
+	$default_chromosome = $a[0] ;
+}
 
 $from = 1 unless defined $from ;
 unless ( defined $to ) {
@@ -32,6 +69,8 @@ while ( <MAPVIEW> ) {
 	chomp ;
 	my ( $basename , @a ) = split "\t" , $_ ;
 	last unless $basename ;
+	
+	$a[0] = $default_chromosome if $force_single_chromosome ;
 	
 	$basename =~ s|/[12]$|| ;
 	
@@ -86,6 +125,8 @@ sub add_pair {
 		( $seq1 , $seq2 ) = ( $seq2 , $seq1 ) ;
 	}
 	
+	$chr = chr2table ( $chr ) ;
+	
 	my $p = sprintf "%s %12d" , $chr , $pos1 ;
 	
 	if ( $inv ) { # INVERSIONS
@@ -95,6 +136,12 @@ sub add_pair {
 	} else { # PERFECT MATCH
 		print "/*P$p*/ INSERT INTO $chr"."_perfect_match (read_name,pos1,pos2) VALUES (\"$name\",$pos1,$pos2) ;\n" ;
 	}
+}
+
+sub chr2table {
+	my ( $chr ) = shift ;
+	$chr = "chr_$chr" if $chr =~ /^\d/ ; # Avoid table names starting with numbers
+	return $chr ;
 }
 
 # Add a single read
@@ -108,6 +155,7 @@ sub add_single {
 	return if $pos > $to or $pos < $from ;
 
 	my $seq = get_match ( $d1 ) ;
+	$chr = chr2table ( $chr ) ;
 	my $p = sprintf "%s %12d" , $chr , $pos ;
 	print "/*X$p*/ INSERT INTO $chr"."_single (read_name,pos1,seq1) VALUES (\"$name\",$pos,\"$seq\") ;\n" ;
 }
@@ -148,6 +196,9 @@ sub get_reference_from_file {
 			$lchr = substr ( $_ , 1 ) ;
 			$lchr =~ s/^\s+// ;
 			$lchr =~ s/\s+$// ;
+			$lchr =~ /^([\S+]+)/ ;
+			$lchr = $1 ;
+			$lchr =~ s/[^A-Za-z0-9]/_/g ;
 		} else { # Otherwise, DNA
 			$s .= uc $_ ;
 		}
@@ -161,6 +212,7 @@ sub create_tables {
 	print "$dbinit CREATE TABLE meta ( key VARCHAR[64] , value VARCHAR[256] );\n" ;
 	print "$dbinit CREATE TABLE chromosomes ( name VARCHAR[256] , size INTEGER );\n" ;
 	foreach my $chr ( sort keys %chrs ) {
+		$chr = chr2table ( $chr ) ;
 		print "$dbinit CREATE TABLE $chr" . "_perfect_match ( read_name VARCHAR[32], pos1 INTEGER, pos2 INTEGER );\n" ;
 		print "$dbinit CREATE TABLE $chr" . "_inversions ( read_name VARCHAR[32], pos1 INTEGER, seq1 VARCHAR[64] , pos2 INTEGER , seq2 VARCHAR[64] );\n" ;
 		print "$dbinit CREATE TABLE $chr" . "_single ( read_name VARCHAR[32], pos1 INTEGER, seq1 VARCHAR[64] );\n" ;
@@ -180,7 +232,9 @@ sub finish_db {
 	print "/*ZZ*/ COMMIT;\n" ;
 	print "$dbend BEGIN EXCLUSIVE TRANSACTION;\n" ;
 	foreach my $chr ( sort keys %chrs ) {
-		print "$dbend2 INSERT INTO chromosomes ( name , size ) VALUES ( \"$chr\"  , " . length ( $chrs{$chr} ) . " ) ;\n" ;
+		my $ochr = $chr ;
+		$chr = chr2table ( $chr ) ;
+		print "$dbend2 INSERT INTO chromosomes ( name , size ) VALUES ( \"$chr\"  , " . length ( $chrs{$ochr} ) . " ) ;\n" ;
 		print "$dbend2 CREATE INDEX $chr"."_sin_index ON $chr"."_single ( pos1 );\n" ;
 		print "$dbend2 CREATE INDEX $chr"."_snp_index ON $chr"."_snp_match ( pos1 , pos2 );\n" ;
 		print "$dbend2 CREATE INDEX $chr"."_inv_index ON $chr"."_inversions ( pos1 , pos2 );\n" ;
